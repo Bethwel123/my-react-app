430ef5321a042d43070338f896885e2cc1f898fb
Bethwel123 <bethwellangat654@gmail.com>
2024-08-03 01:37:47 +0300

    Added react facts

---
+++ src/Components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Main = () => (
+  <div className="div-container">
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has over 100k stars on GitHub</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+);
+
+export default Main;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../images/logo192.png";
+
+const Navbar = () => (
+  <div className="navbar">
+    <img src={logo} alt="React logo" />
+    <h3>ReactFacts</h3>
+    <h4>React Course-Project 1</h4>
+  </div>
+);
+
+export default Navbar;
